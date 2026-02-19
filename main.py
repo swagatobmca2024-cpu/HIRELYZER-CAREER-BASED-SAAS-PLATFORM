0ce1786ea9764ae921e34efd90a2d49dc0090e2b
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2026-02-20 01:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6808,7 +6808,6 @@ with tab2:
             <a href="https://www.sejda.com/html-to-pdf" target="_blank" style="color:#2f4f6f; text-decoration:none;">
             convert it to PDF using Sejda's free online tool</a>.
             """, unsafe_allow_html=True)
-
 FEATURED_COMPANIES = {
     "tech": [
         {
