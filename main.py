9ad9ea2e30e9e6cf28ec2b7cb08b456a8cce4962
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2026-03-21 04:14:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19901,6 +19901,4 @@ if tab5:
 			<p>Last updated: {}</p>
 		</div>
 		""".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), unsafe_allow_html=True)
-			<p>Last updated: {}</p>
-		</div>
-		""".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), unsafe_allow_html=True)
+				
