a1be25af25573d9be38e1a2728f13207ca179836
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2026-05-18 00:49:22 +0530

    Update tab3_ui.py

--- tab3_ui.py
+++ tab3_ui.py
@@ -1403,10 +1403,10 @@ def _job_search_interactive():
                         st.markdown(_card_html, unsafe_allow_html=True)
 
                     with delete_col:
-                        # Delete button
                         if st.button("🗑", key=f"delete_{search['id']}", help="Delete this search"):
-                            delete_saved_job_search(search['id'])
-                            st.rerun(scope="fragment")
+                            success = delete_saved_job_search(search['id'])
+                            if success:
+                                st.rerun(scope="fragment")
             else:
                 st.markdown(f"""
 <div class="empty-state">
